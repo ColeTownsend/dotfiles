4048c691295f9dbe5b577abb43f39a27650e1d80
Adam A <adam@fapfap.se>
2014-11-16 02:26:01 +0100

    Add emacs-live config.

---
+++ .emacs-live.el
@@ -0,0 +1,12 @@
+;; Load core packs
+(live-use-packs '(stable/foundation-pack
+                  stable/lang-pack
+                  stable/git-pack
+                  stable/power-pack
+                  stable/org-pack
+                  stable/clojure-pack
+                  stable/bindings-pack))
+
+;; Load custom packs
+(live-add-packs '(~/.live-packs/install-packages-pack))
+(live-add-packs '(~/.live-packs/aaa-pack))
